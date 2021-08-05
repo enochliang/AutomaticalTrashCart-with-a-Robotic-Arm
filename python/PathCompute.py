c3952196e76c29fb6c85c1e82a394acc03bf59eb
Enoch Liang <liang.enoch0819@gmail.com>
2021-08-05 16:27:44 +0800

    Delete python directory

--- python/PathCompute.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import GaussianElimination as GE
-
-def ARM_curve(Angle0,Anglef):##"""Provide initial Angle and final Angle to obtain the coefficient of the cube polynomial."""
-    T=150.0
-    A=np.array([[0.0,0.0,0.0,1.0],[T*T*T,T*T,T,1.0],[0.0,0.0,1.0,0.0],[3.*T*T,2.*T,1.0,0.0]])
-    b=np.array([[Angle0],[Anglef],[0.0],[0.0]])
-    return GE.GEPP(A, b)
